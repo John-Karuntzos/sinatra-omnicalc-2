ba182925c97bac21fc64cce2059330dfab9691d0
John Karuntzos <61172885+John-Karuntzos@users.noreply.github.com>
2024-03-23 16:42:10 +0000

    add fixed

--- app.rb
+++ app.rb
@@ -5,13 +5,13 @@ get("/") do
   redirect("/add")
 end
 
-get("/ad") do
+get("/add") do
   erb(:add_form)
 end
 
 get("/wizard_add") do
-  @first_num = params.fetch("first_num").to_f
-
+  @first_num = params.fetch("first_number").to_f
+  @second_num = params.fetch("second_number").to_f
   @result = @first_num + @second_num
   erb(:add_result)
 end
@@ -31,7 +31,7 @@ get("/multiply") do
   erb(:multiplication_form)
 end
 
-get("/wizard multiply") do
+get("/wizard_multiply") do
   erb(:mult_result)
 end
 
